ee790a2cf3a69efdb5a6f4ec0ab41aa6348bfdaa
lingga <linggaadi4nd@gmail.com>
2020-03-15 17:26:57 +0700

    update(cronNode) add 23 hours cron

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 # 2019-nCov
 [Twitter](https://twitter.com/Linggaadi4)
 
-A simple BOT App to get the latest information about the corona virus that is spreading in Indonesia and world, bot send data only always 24 hours
+A simple BOT App to get the latest information about the corona virus that is spreading in Indonesia and world, bot send data only always 23 hours
 
 ## Prerequisites
 - Twitter developer App and its corresponding consumer key/secret and access token key/secret (you can used your account twitter )
--- index.js
+++ index.js
@@ -46,7 +46,7 @@ const indonesia = async() => {
                 } else if (content.length > 0) {
                     await tweet(content, start_tweet.id_str).then()
                 }
-                redis_client.setex('news:'+hash_code, 86400*24*30, c.title)
+                redis_client.setex('news:'+hash_code, 60*24*30, c.title)
             }
         }
     })
@@ -57,6 +57,92 @@ const indonesia = async() => {
             if(!checkExist || checkExist !== json_str) {
                 await tweet(`2019-nCov Update for ${t.country}\n\nInfections: ${t.infection}\nActive cases: ${t.active_cases}\nDeaths: ${t.deaths}\nRecovered: ${t.recovered}\n\nUpdated ${new Date().toLocaleString()}`)
                 redis_client.set('indonesia_affected', json_str)
+            }
+        }
+    })
+}
+
+const malaysia = async() => {
+    var corona = await covid19_update()
+    corona.news.every(async c => {
+        var content = c.content
+        if(c.title.toLowerCase().includes('malaysia')) {
+            var hash_code = crc32(c.title)
+            if(await redisGet('news:'+hash_code)) {
+                console.log(hash_code+" EXIST")
+                return false
+            } else {
+                // console.log(hash_code)
+                var start_tweet = await tweet(c.title).then(start_tweet)
+                var latest_id = start_tweet.id_str
+                if(content.length > 278) {
+                    var chunk = chunkText(content)
+                    for (let i = 0; i < chunk.length; i++) {
+                        const element = chunk[i];
+                        var text = element.join(' ')
+                        if(i != chunk.length - 1) {
+                            text += ` ~(${i+1}/${chunk.length})`
+                        }
+                        var child_tweet = await tweet(text, latest_id).then(child_tweet)
+                        latest_id = child_tweet.id_str
+                    }
+                } else if (content.length > 0) {
+                    await tweet(content, start_tweet.id_str).then()
+                }
+                redis_client.setex('news:'+hash_code, 60*24*30, c.title)
+            }
+        }
+    })
+    corona.regions_affected.every(async t => {
+        if(t.country.toLowerCase().includes('malaysia')) {
+            let json_str = JSON.stringify(t)
+            var checkExist = await redisGet('malaysia_affected')
+            if(!checkExist || checkExist !== json_str) {
+                await tweet(`2019-nCov Update for ${t.country}\n\nInfections: ${t.infection}\nActive cases: ${t.active_cases}\nDeaths: ${t.deaths}\nRecovered: ${t.recovered}\n\nUpdated ${new Date().toLocaleString()}`)
+                redis_client.set('malaysia_affected', json_str)
+            }
+        }
+    })
+}
+
+const singapore = async() => {
+    var corona = await covid19_update()
+    corona.news.every(async c => {
+        var content = c.content
+        if(c.title.toLowerCase().includes('singapore')) {
+            var hash_code = crc32(c.title)
+            if(await redisGet('news:'+hash_code)) {
+                console.log(hash_code+" EXIST")
+                return false
+            } else {
+                // console.log(hash_code)
+                var start_tweet = await tweet(c.title).then(start_tweet)
+                var latest_id = start_tweet.id_str
+                if(content.length > 278) {
+                    var chunk = chunkText(content)
+                    for (let i = 0; i < chunk.length; i++) {
+                        const element = chunk[i];
+                        var text = element.join(' ')
+                        if(i != chunk.length - 1) {
+                            text += ` ~(${i+1}/${chunk.length})`
+                        }
+                        var child_tweet = await tweet(text, latest_id).then(child_tweet)
+                        latest_id = child_tweet.id_str
+                    }
+                } else if (content.length > 0) {
+                    await tweet(content, start_tweet.id_str).then()
+                }
+                redis_client.setex('news:'+hash_code, 60*24*30, c.title)
+            }
+        }
+    })
+    corona.regions_affected.every(async t => {
+        if(t.country.toLowerCase().includes('singapore')) {
+            let json_str = JSON.stringify(t)
+            var checkExist = await redisGet('singapore_affected')
+            if(!checkExist || checkExist !== json_str) {
+                await tweet(`2019-nCov Update for ${t.country}\n\nInfections: ${t.infection}\nActive cases: ${t.active_cases}\nDeaths: ${t.deaths}\nRecovered: ${t.recovered}\n\nUpdated ${new Date().toLocaleString()}`)
+                redis_client.set('singapore_affected', json_str)
             }
         }
     })
@@ -89,11 +175,11 @@ const world = async() => {
                 } else if (content.length > 0) {
                     await tweet(content, start_tweet.id_str).then()
                 }
-                redis_client.setex('news:'+hash_code, 86400*24*30, c.title)
+                redis_client.setex('news:'+hash_code, 60*24*30, c.title)
             }
         }
     })
-    corona.regions_affected.every(async t => {
+    corona.total_affected.every(async t => {
         if(t.country.toLowerCase().includes('total')) {
             let json_str = JSON.stringify(t)
             var checkExist = await redisGet('total_ffected')
@@ -106,10 +192,13 @@ const world = async() => {
 }
 
 
-cron.schedule("* * * * *", () => {
+cron.schedule("0 0 */23 * * *", () => {
+    console.log('running a task every 23 hours');
     console.log("START")
-    indonesia()
     world()
+    indonesia()
+    singapore()
+    malaysia()
 })
 
 console.log("Service is running, press CTRL+C to stop.")
